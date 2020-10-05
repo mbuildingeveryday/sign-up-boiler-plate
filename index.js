d72608d4ccdb49e91e775968f8adb6873ff1b4b0
minki kim <mbuildingeveryday@gmail.com>
2020-10-05 13:34:48 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,15 +2,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+const config = require('./config/key');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://dbUser:as123@cluster0.u1mfo.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..."))
 .catch(err => console.log(err))
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({"success": false, err})
+    return res.status(700).json({
+      "success": true
+    })
+  })
 })
 
 app.listen(port, () => {
